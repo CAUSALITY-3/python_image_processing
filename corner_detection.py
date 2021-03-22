215920c644d6ca3c66b40b7d6ee5c0a0aa3c453f
CAUSALITY-3 <70097078+CAUSALITY-3@users.noreply.github.com>
2021-03-22 16:38:19 +0530

    Update corner_detection.py

--- corner_detection.py
+++ corner_detection.py
@@ -3,22 +3,16 @@ import numpy as np
 import cv2
 
 img = cv2.imread('2.png')
-img = cv2.resize(img, (0, 0), fx=0.75, fy=0.75)
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img = cv2.resize(img, (0, 0), fx=0.75, fy=0.75)     #resized image to fit into the screen
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)        #changed cocor to gray scale
 
-corners = cv2.goodFeaturesToTrack(gray, 100, 0.01, 10)
-corners = np.int0(corners)
+corners = cv2.goodFeaturesToTrack(gray, 100, 0.01, 10)   #Shi-Tomasi Corner Detector & Good Features to Track method used here
+                                                         #(gray, 100, .01, 10) = (source image, no. of corners, accuracy(0-1), min eucleadian distance btw corners)
+corners = np.int0(corners)                                
 
 for corner in corners:
-	x, y = corner.ravel()
-	cv2.circle(img, (x, y), 5, (255, 0, 0), -1)
-
-for i in range(len(corners)):
-	for j in range(i + 1, len(corners)):
-		corner1 = tuple(corners[i][0])
-		corner2 = tuple(corners[j][0])
-		color = tuple(map(lambda x: int(x), np.random.randint(0, 255, size=3)))
-		cv2.line(img, corner1, corner2, color, 1)
+	x, y = corner.ravel()                            #[[x,y]].ravel=x,y
+	cv2.circle(img, (x, y), 5, (255, 0, 0), -1)      #(img, (x, y), 5, (255, 0, 0), -1) = (source image, point to draw circle, radius, BGR, fill the circle)
 
 cv2.imshow('Frame', img)
 cv2.waitKey(0)
